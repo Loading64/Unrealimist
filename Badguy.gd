f13871911da5a94517de8dcb8c7e83762da5cd75
UsernameLoading <Oldinglogan@gmail.com>
2021-06-16 14:43:24 +1200

    added models for enemy and player viewmodel

--- Badguy.gd
+++ Badguy.gd
@@ -1,19 +1,9 @@
 extends KinematicBody
-var health = 150
+var enemy_health = 150
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 func _process(delta):
-	if health <= 0:
+	if enemy_health <= 0:
 		print("enemy dead")
 		queue_free()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
---
+++ enemyanimations.gd
@@ -0,0 +1,9 @@
+extends Spatial
+
+onready var anim_player = $AnimationPlayer
+
+func _process(delta):
+	anim_player.play("mixamocom")
+
+
+
